43e8dd90ac0c3541ebd8752f5759334f2885be99
Simone Romei <simone.romei@sap.com>
2023-12-14 17:44:25 +0100

    added support for docker tag on push based on env.sh

--- docker/scripts/env.sh
+++ docker/scripts/env.sh
@@ -8,3 +8,6 @@ export SPARTACUS_IMAGE_NODE="spartacus/node"
 
 # The name of the docker image for spartacus Nginx (ex. spartacus/nginx)
 export SPARTACUS_IMAGE_NGINX="spartacus/nginx"
+
+# The name of the docker tag to use (ex. 1.0.0)
+export SPARTACUS_IMAGE_TAG="1.0.0"
